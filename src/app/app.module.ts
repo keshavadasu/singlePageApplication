f277608c2ae2ee8407485c8cb4bd554de27b8cae
TarunKala <tarunk.kala@hotmail.com>
2018-07-30 12:41:05 +0530

    app commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { ServicesComponent } from './services/services.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent,
+    ServicesComponent,
+    AboutusComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: "", component: HomeComponent },
+      { path: "home", component: HomeComponent },
+      { path: "register", component: RegisterComponent },
+      { path: "login", component: LoginComponent },
+      { path: "services", component: ServicesComponent },
+      { path: "aboutus", component: AboutusComponent }])
   ],
   providers: [],
   bootstrap: [AppComponent]
